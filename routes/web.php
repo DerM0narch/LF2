170577e61843076978bb2589d18592e1b870c741
Xervia <25280953+Xervia@users.noreply.github.com>
2021-01-06 10:06:49 +0100

    change front-end

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/app', function () {
+    return view('app');
+})->name('app');
 
 
 Route::group(['prefix' => 'admin'], function () {
