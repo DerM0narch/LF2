3de1767109590344b78922f0296bf6d86c47c5e0
Thies Mextorf <maxomaniac@googlemail.com>
2021-01-06 18:06:08 +0100

    flex item

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
@@ -40,7 +40,7 @@
                 @endif
                 <br>
 
-               </div> 
+                <div class="flex items-center mt-4">
                 @if (Route::has('register'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
                          {{ __('Register') }}
